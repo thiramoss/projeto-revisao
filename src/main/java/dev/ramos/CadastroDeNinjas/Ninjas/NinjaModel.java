9ed564aeda8f5617c4328a70c4e80a8998219fc8
thiramoss <trs.tiba@gmail.com>
2026-04-12 17:43:57 -0300

    feat: Migrations para tb_cadastro incluindo a column de rank

--- src/main/java/dev/ramos/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/ramos/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -23,6 +23,9 @@ public class NinjaModel {
     @Column(unique = true)
     private String email;
 
+    @Column(name = "rank")
+    private String rank;
+
     @Column (name = "img_url")
     private String imgUrl;
 
---
+++ src/main/resources/db/migrations/V2__Add_rank_tb_cadastro.sql
@@ -0,0 +1,3 @@
+-- V1: Migrations para adicionar a coluna de RANK na tabela de cadastros
+ALTER TABLE tb_cadastro
+ADD COLUMN rank VARCHAR(255);
