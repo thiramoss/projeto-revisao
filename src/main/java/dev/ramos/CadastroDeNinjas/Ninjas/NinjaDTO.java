ada5b3664452d84c68173fac6f6e80a6f7e37b3e
thiramoss <trs.tiba@gmail.com>
2026-04-21 09:45:36 -0300

    chore:Inclui o DTO e mapeia manualmente o Mapper

--- src/main/java/dev/ramos/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/ramos/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,18 @@
 package dev.ramos.CadastroDeNinjas.Ninjas;
+import dev.ramos.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private String rank;
+    private String imgUrl;
+    private int idade;
+    private MissoesModel missoes;
 }
--- src/main/java/dev/ramos/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/ramos/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -13,9 +13,18 @@ import java.util.Optional;
 public class NinjaService {
 
     private NinjaRepository ninjaRepository;
+    private NinjaMapper ninjaMapper;
 
-    public NinjaService(NinjaRepository ninjaRepository) {
+    public NinjaService(NinjaRepository ninjaRepository, NinjaMapper ninjaMapper) {
         this.ninjaRepository = ninjaRepository;
+        this.ninjaMapper = ninjaMapper;
+    }
+
+    // Criar um novo ninja
+    public NinjaDTO criarNinja(NinjaDTO ninjaDTO){
+        NinjaModel ninja = ninjaMapper.map(ninjaDTO);
+        ninja = ninjaRepository.save(ninja);
+        return ninjaMapper.map(ninja);
     }
 
     // Listar todos os ninjas
@@ -27,11 +36,6 @@ public class NinjaService {
     public NinjaModel listarNinjasPorId(Long id) {
         Optional<NinjaModel> ninjaPorId = ninjaRepository.findById(id);
         return ninjaPorId.orElse(null);
-    }
-
-    // Criar um novo ninja
-    public NinjaModel criarNinja(NinjaModel ninja){
-        return ninjaRepository.save(ninja);
     }
 
     // Deletar o ninja - Tem que ser método void
